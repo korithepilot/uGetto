b045ff23687ec414ea8f4364f0dc12b7fd203149
Hugyik Kornél <40920279+korithepilot@users.noreply.github.com>
2022-09-15 20:10:13 +0200

    now with Rick Astley™

--- uGetto/melodyplayer/melodyplayer.ino
+++ uGetto/melodyplayer/melodyplayer.ino
@@ -1,44 +1,40 @@
 static uint8_t i;
 static uint8_t j;
 
-#define T_MIN 10
+#define T_MIN = 17
 #define  BUZZER_PIN  PB0
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
 #include <util/delay.h>
+
 //#include "original_sound.h"
+
 #include "sound_type.h"
 #include "sound_constants.h"
 #include "sound_generation.h"
-#include "song.h"
-
-
-
+#include "rick.h"
 
 int main(void)
 {
-
-	/* setup */
 	DDRB = 0b00000001; // set BUZZER pin as OUTPUT
 	PORTB = 0b00000000; // set all pins to LOW
 	TCCR0A |= (1<<WGM01); // set timer mode to Fast PWM
 	TCCR0A |= (1<<COM0A0); // connect PWM pin to Channel A of Timer0
 
 		_delay_ms(2000);
-	/* Walk throwgh all octaves */
+    
+	// Walk through all octaves
 	for (i = 3; i < 8; ++i) {
-		for (j = 0; j < 12; ++j) {
+		for (j = 0; j < 12; ++j)
             playNote(12*i+j, 8);
-		}
 
 		_delay_ms(2000);
 	}
 
 	stop();
 	 _delay_ms(1500);
-
 
 	while (1) {
 		for(j = 0; j < SONGLENGTH; j++)
--- uGetto/melodyplayer/sound_generation.h
+++ uGetto/melodyplayer/sound_generation.h
@@ -1,7 +1,7 @@
 static inline void tone(uint8_t noteNum)
 {
-	TCCR0B = (TCCR0B & ~((1<<CS02)|(1<<CS01)|(1<<CS00))) | compares[noteNum];
-  	OCR0A =  (noteNum <= 23) ? N_64 : N_8 - 1; // set the OCRnx
+	TCCR0B = (TCCR0B & ~((1<<CS02)|(1<<CS01)|(1<<CS00))) | compares[noteNum];  // set compare
+  	OCR0A =  (noteNum <= 23) ? N_64 : N_8 - 1; // set the scaler
 }
 
 static inline void stop(void)
@@ -14,18 +14,18 @@ static void inline playNote(const uint8_t& noteNum, const int& duration_cycles)
 {
     if(noteNum == 255)
     {
-	    TCCR0B &= ~((1<<CS02)|(1<<CS01)|(1<<CS00)); // stop the timer
+	    TCCR0B &= ~((1<<CS02)|(1<<CS01)|(1<<CS00)); // stop
     }
     else
     {
-	    TCCR0B = (TCCR0B & ~((1<<CS02)|(1<<CS01)|(1<<CS00))) | compares[noteNum];
-  	    OCR0A =  (noteNum <= 23) ? N_64 : N_8 - 1;
+	    TCCR0B = (TCCR0B & ~((1<<CS02)|(1<<CS01)|(1<<CS00))) | compares[noteNum];  // set compare
+  	    OCR0A =  (noteNum <= 23) ? N_64 : N_8 - 1; // set the scaler
     }
 
     i = duration_cycles;
     while(i-- > 0)
     {
-        _delay_ms(T_MIN);
+        _delay_ms(T_MIN); // _delay_ms needs a compile time constant, this is why it was done this way
     }
 }
 
@@ -33,12 +33,12 @@ static void inline playNote(const Sound& s)
 {
     if(s.noteNum == 255)
     {
-	    TCCR0B &= ~((1<<CS02)|(1<<CS01)|(1<<CS00)); // stop the timer
+	    TCCR0B &= ~((1<<CS02)|(1<<CS01)|(1<<CS00)); // stop
     }
     else
     {
-	    TCCR0B = (TCCR0B & ~((1<<CS02)|(1<<CS01)|(1<<CS00))) | compares[s.noteNum];
-  	    OCR0A =  (s.noteNum <= 23) ? N_64 : N_8 - 1;
+	    TCCR0B = (TCCR0B & ~((1<<CS02)|(1<<CS01)|(1<<CS00))) | compares[s.noteNum];  // set compare
+  	    OCR0A =  (s.noteNum <= 23) ? N_64 : N_8 - 1; // set the scaler
     }
 
     i = s.duration_cycles;
